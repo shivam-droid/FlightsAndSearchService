14a464d776515854eb39fa70b4ef7a81fad1a0e1
shivam <shivam.singhal.211@gmail.com>
2025-05-10 12:06:05 +0530

    Added Airplane table

---
+++ src/seeders/20250510062904-add-airplanes.js
@@ -0,0 +1,57 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert('Airplanes', [
+      {
+        modelNumber: 'Boeing 737',
+        capacity: 200,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        modelNumber: 'Airbus A320',
+        capacity: 180,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        modelNumber: 'Boeing 777',
+        capacity: 300,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        modelNumber: 'Airbus A380',
+        capacity: 500,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        modelNumber: 'Boeing 747',
+        capacity: 400,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ])
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
